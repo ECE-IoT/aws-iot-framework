35766f8d1ed890b6b369080bf3d70bab32db7f39
t3mschm2d <tim.schmid1313@gmail.com>
2022-01-24 21:46:49 +0000

    ADD filter function for Alexa Skill

--- lambdas/AlexaSkillLambda/prompts.py
+++ lambdas/AlexaSkillLambda/prompts.py
@@ -1,3 +1,7 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+import os
+
 # Response messages
 WELCOME_MESSAGE = "Hi, welcome to our IOT Framework, you can ask me about your the actual value of your sensors!"
 HELP_MESSAGE = "You can ask me about a specific value?"
@@ -7,19 +11,69 @@ FALLBACK_REPROMPT = "What can I help you with"
 ERROR_MESSAGE = "Sorry, an error occurred."
 STOP_MESSAGE = "Ok Bye"
 
+# Query specific constants
+DYNAMO_DB = 'dynamodb'
+DYN_DYB_TABLE = "TopicTable"
+TIMESTREAM_DB = "measurement_fleet"
+ATTR_ESP_MAC = 'ESP_MAC'
+ATTR_ESP_ID = 'ESP_ID'
 
-# Provides senor details
-def get_sensor_info(sensor_id):
-    sensor = sensor_id.lower().replace('-', '').replace(' ', '')
-    if sensor == '1':
-        return ''
-    elif sensor == '2':
-        return ''
-    elif sensor == '3':
-        return ''
-    elif sensor == '4':
-        return ''
-    elif sensor == '5':
-        return ' '
+
+def response(sensor_type, position):
+    if sensor_type:
+        fetch_sensor_response(sensor_type, position)
+
+
+def fetch_sensor_response(sensor_type, position):
+    measured_object = get_sensor_value('temperature', 'RoomX')
+    value = float()
+
+    for val in measured_object:
+        value = value + round(float(val['Value']))
+
+    value = value / len(measured_object)
+    name = measured_object[0]['SensorType']
+    unit = measured_object[0]['Unit']
+
+    print(f"The {name} is {value} {unit}")
+
+
+def get_sensor_value(sensor_type, position):
+    timestream_client = boto3.client('timestream-query')
+
+    region = os.environ.get('Region', 'eu-central-1')
+
+    if region == "eu-central-1":
+        dynamodb_table = boto3.resource(DYNAMO_DB, region_name=region)
     else:
-        return 'No data found'
+        return ERROR_MESSAGE
+
+    dynamo = dynamodb_table.Table(DYN_DYB_TABLE)
+    filtered_dynamodb = dynamo.scan()['Items']
+    devices = list()
+    query_response = list()
+
+    for items in filtered_dynamodb:
+        devices.append(items[ATTR_ESP_ID])
+
+    for device in devices:
+        data_value = list()
+        column_list = list()
+        query = f"SELECT * FROM {TIMESTREAM_DB}.{device} WHERE SensorType = '{sensor_type}' LIMIT 1"
+        result = timestream_client.query(QueryString=query)
+
+        data = result['Rows'][0]['Data']
+        for d in data:
+            data_value.append(d['ScalarValue'])
+
+        column_info = result['ColumnInfo']
+        for col in column_info:
+            column_list.append(col['Name'])
+
+        query_response.append(convert_to_dict(column_list, data_value))
+
+    return query_response
+
+
+def convert_to_dict(key_list, value_list):
+    return {key_list[i]: value_list[i] for i in range(len(key_list))}
